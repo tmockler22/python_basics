6b740a6380792298ad4beaecc26ce913bb4fa823
Tristan <tmocklercoding@gmail.com>
2020-06-18 11:27:29 -0500

    Basic input

--- basics.py
+++ basics.py
@@ -1,11 +1,19 @@
-import datetime
+# import datetime
 
-mynow = datetime.datetime.now()
-print("The date and time is", mynow)
+# mynow = datetime.datetime.now()
+# print("The date and time is", mynow)
 
-x = 10
-y = "10"
+# x = 10
+# y = "10"
 
-sum1 = x + x
-sum2 = y + y
+# sum1 = x + x
+# sum2 = y + y
 
+u_input = ""
+response = ""
+
+while u_input != "/end":
+  response = response + u_input
+  u_input = input("Say something: ")
+  
+print(response)
